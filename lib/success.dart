1da675ebe53c9a580e2b6fd6e9188199f7c0e8d9
yasui-kohei <yasui.kohei.xj@alumni.tsukuba.ac.jp>
2022-12-12 00:32:54 +0900

    成功画面の修正
    さんや

--- lib/success.dart
+++ lib/success.dart
@@ -5,8 +5,10 @@ class SuccessScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('成功!!'),
+    return const Scaffold(
+      body: Center(
+        child: Text('成功!!'),
+      ),
     );
   }
 }
