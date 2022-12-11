3dc164b28446b48395ab34233eac04b39186f79c
yasui-kohei <yasui.kohei.xj@alumni.tsukuba.ac.jp>
2022-12-12 00:33:34 +0900

    名前の最大長を５文字に変更
    さんや

--- lib/validators/custom_validators.dart
+++ lib/validators/custom_validators.dart
@@ -6,7 +6,7 @@ final nameValidator = MultiValidator([
   // 必須入力チェック
   RequiredValidator(errorText: "名前を入力してください"),
   // 最大長チェック
-  MaxLengthValidator(20,errorText: "名前は20文字以内で入力してください")
+  MaxLengthValidator(5,errorText: "名前は5文字以内で入力してください")
 ]);
 
 final instagramValidator = MultiValidator([
